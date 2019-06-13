a0e65a7a4b9fb6cc9c43edac22dd6bb57dd1fd7d
dongchao <dongchao@mistong.com>
2019-06-13 13:08:47 +0800

    增加方法

--- src/main/java/com/hopedc/learn/stream/normal/Example.java
+++ src/main/java/com/hopedc/learn/stream/normal/Example.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Example {
@@ -17,7 +18,8 @@ public class Example {
     }
 
     public static void main(String[] args) {
-
+        getListName();
+        getListToMap();
     }
 
     private static void getListName(){
@@ -30,9 +32,26 @@ public class Example {
         System.out.println("methodStrs:"+JSON.toJSONString(methodStrs));
 
         // stream方式
-        List<String> methodStrsStream = requestMethods.stream().map(RequestMethod::getName).collect(Collectors.toList());
+        List<String> methodStrsStream = requestMethods.stream().map(RequestMethod::getName)
+                .collect(Collectors.toList());
 
         System.out.println("methodStrsStream:"+JSON.toJSONString(methodStrsStream));
     }
+
+    /**
+     * List转Map，非常常见的场景
+     */
+    private static void getListToMap(){
+        // 其中key和value都为字段
+        Map<Integer, String> result = requestMethods.stream()
+                .collect(Collectors.toMap(RequestMethod::getArgsNum, RequestMethod::getName));
+        System.out.println("getListToMap:"+JSON.toJSONString(result));
+        // key为字段value为list内对象
+        Map<String, RequestMethod> partsMap = requestMethods.stream()
+                .collect(Collectors.toMap(k -> k.getName()+k.getArgsNum(), part -> part));
+        System.out.println("partsMap:"+JSON.toJSONString(partsMap));
+    }
+
+
 
 }
